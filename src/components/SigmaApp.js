e2881b43a6d6b393f77073733eab065adc9d0040
fourers <fourersxd@gmail.com>
2023-07-29 22:12:44 +1000

    Add import sort

--- src/components/SigmaApp.js
+++ src/components/SigmaApp.js
@@ -1,8 +1,24 @@
-import React from 'react';
-import Graph from 'graphology';
-import { SigmaContainer } from '@react-sigma/core';
-import graphData from '../lesmis.json';
 import '@react-sigma/core/lib/react-sigma.min.css';
+
+import { SigmaContainer } from '@react-sigma/core';
+import Graph from 'graphology';
+import React from 'react';
+
+import graphData from '../lesmis.json';
+
+const getHexCode = (rgbStr) => {
+    const hex = rgbStr
+        .replace('rgb(', '')
+        .replace(')', '')
+        .split(',')
+        .map(Number)
+        .map((x) => {
+            const code = x.toString(16);
+            return code.length === 1 ? `0${code}` : code;
+        })
+        .join('');
+    return `#${hex}`;
+};
 
 const generateGraph = (data) => {
     const graph = new Graph();
@@ -12,10 +28,14 @@ const generateGraph = (data) => {
             y: node.y,
             label: node.label,
             size: node.size,
+            color: getHexCode(node.color),
         });
     });
     data.edges.forEach((edge) => {
-        graph.addEdge(edge.source, edge.target, { weight: edge.size });
+        graph.addEdge(edge.source, edge.target, {
+            color: getHexCode(edge.color),
+            weight: edge.size,
+        });
     });
     return graph;
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import SigmaApp from './components/SigmaApp';
 
 const router = createBrowserRouter([
