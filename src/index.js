3e2cd7ff6a608f88fa4762b0edbe8a3a3729cb37
Tony Huynh <fourersxd@gmail.com>
2023-07-30 13:15:46 +1000

    Remove router

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import SigmaApp from './components/SigmaApp';
 
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <SigmaApp />,
-    },
-]);
-
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<RouterProvider router={router} />);
+root.render(<SigmaApp />);
