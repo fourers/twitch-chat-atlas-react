37c93eef30bff948ce7cf4fb896743cd37da8639
Tony Huynh <fourersxd@gmail.com>
2023-07-30 14:41:03 +1000

    Add title box

--- src/components/SigmaInfoControl.js
+++ src/components/SigmaInfoControl.js
@@ -14,10 +14,15 @@ export default function SigmaInfoControl({ graph }) {
             clickStage: () => setCurrentNode(null),
         });
     }, [registerEvents]);
-    return currentNode !== null ? (
+    return currentNode === null ? (
+        <div className="info">
+            <h4>Twitch Chat Atlas <br></br> 22-28 July 2023</h4>
+            <p>{graph.nodes().length} total chatters</p>
+        </div>
+    ) : (
         <div className="info">
             <h4>{graph.getNodeAttribute(currentNode, 'label')}</h4>
             <p>{graph.getNodeAttribute(currentNode, 0)} total messages</p>
         </div>
-    ) : null;
+    );
 }
